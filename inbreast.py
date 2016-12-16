1a7d6706fc1ddf6bab4d22d91e482305285826ba
Liangjian Chen <kuzphi@gmail.com>
2016-12-15 16:33:33 -0800

    w

--- inbreast.py
+++ inbreast.py
@@ -1,22 +1,23 @@
-#import dicom # some machines not install pydicom
+import dicom # some machines not install pydicom
+import gzip
 import scipy.misc
 import numpy as np 
 from sklearn.model_selection import StratifiedKFold
 import cPickle
-#import matplotlib
-#import matplotlib.pyplot as plt 
-from skimage.filters import threshold_otsu
+import matplotlib
+import matplotlib.pyplot as plt 
+from skimage.filter import threshold_otsu
 import os
 from os.path import join as join
 import csv
 import scipy.ndimage
 #import cv2
-path = './AllDICOMs/'
-preprocesspath = './preprocesspath/'
-labelfile = './label.txt'
+path = '../trainingData/'
+preprocesspath = '../preprocessedData/'
+labelfile = '../label.txt'
 
 def readlabel():
-  '''read the label as a dict from labelfile'''
+	'''read the label as a dict from labelfile'''
 	mydict = {}
 	with open(labelfile, 'r') as f:
 		flines = f.readlines()
@@ -30,40 +31,40 @@ def readlabel():
 	return mydict
 
 def readdicom(mydict):
-  '''read the dicom image, rename it consistently with the name in labels, crop and resize, and save as pickle.
-  mydict is the returned value of readlabel'''
-	img_ext = '.dcm'
-	img_fnames = [x for x in os.listdir(path) if x.endswith(img_ext)]
-	for f in img_fnames:
-		names = f.split('_')
-		if names[0] not in mydict:
-		  print(names[0]+'occur error')
-
-		dicom_content = dicom.read_file(join(path,f))
+	'''read the dicom image, rename it consistently with the name in labels, crop and resize, and save as pickle.
+	mydict is the returned value of readlabel'''
+	img_ext = '.dcm.gz'
+	# print os.listdir(mydict)
+	img_fnames = [x for x in os.listdir(mydict) if x.endswith(img_ext)]	
+	for name in img_fnames:
+		print name
+		# with gzip.open(join(path,name),'rb') as infile:
+		# 	dicom_content = dicom.read_file(infile)
+		dicom_content = dicom.read_file(name)
 		img = dicom_content.pixel_array
-		fig = plt.figure()
-		ax1 = plt.subplot(3,3,1)
-		ax2 = plt.subplot(3,3,2)
-		ax3 = plt.subplot(3,3,3)
-		ax4 = plt.subplot(3,3,4)
-		ax5 = plt.subplot(3,3,5)
-		ax6 = plt.subplot(3,3,6)
-		ax7 = plt.subplot(3,3,7)
-		ax8 = plt.subplot(3,3,8)
-		ax9 = plt.subplot(3,3,9)
-		ax1.imshow(img, cmap='Greys_r')
-		ax1.set_title('Original')
-		ax1.axis('off')
+		# fig = plt.figure()
+		# ax1 = plt.subplot(3,3,1)
+		# ax2 = plt.subplot(3,3,2)
+		# ax3 = plt.subplot(3,3,3)
+		# ax4 = plt.subplot(3,3,4)
+		# ax5 = plt.subplot(3,3,5)
+		# ax6 = plt.subplot(3,3,6)
+		# ax7 = plt.subplot(3,3,7)
+		# ax8 = plt.subplot(3,3,8)
+		# ax9 = plt.subplot(3,3,9)
+		# ax1.imshow(img, cmap='Greys_r')
+		# ax1.set_title('Original')
+		# ax1.axis('off')
 
 		thresh = threshold_otsu(img)
 		binary = img > thresh
-		ax2.imshow(binary, cmap='Greys_r')
-		ax2.set_title('mask')
-		ax2.axis('off')
+		# ax2.imshow(binary, cmap='Greys_r')
+		# ax2.set_title('mask')
+		# ax2.axis('off')
 
 		minx, miny = 0, 0
 		maxx, maxy = img.shape[0], img.shape[1]
-		for xx in xrange(img.shape[1]):
+		for xx in xrange(img.shape[0]):
 			if sum(binary[xx, :]==0) < binary.shape[1]-60:
 				minx = xx
 				break
@@ -73,61 +74,71 @@ def readdicom(mydict):
 				maxx = xx
 				break
 
-		if names[3] == 'R':
-			maxy = img.shape[1]
-			for yy in xrange(int(img.shape[1]*3.0/4), -1, -1):
-				if sum(binary[:,yy]==0) > binary.shape[0]-10: 
-					miny = yy
-					break
+		# if names[3] == 'R':
+		# 	maxy = img.shape[1]
+		for yy in xrange(img.shape[1]):
+			if sum(binary[:,yy]==0) > binary.shape[0]-10: 
+				miny = yy
+				break
 
-		else:
-			miny = 0
-			for yy in xrange(int(img.shape[1]/4.0), img.shape[1], 1):
-				if sum(binary[:,yy]==0) > binary.shape[0]-10: 
-					maxy = yy
-					break
+		# else:
+		# 	miny = 0
+		for yy in xrange(img.shape[1] - 1, 0, -1):
+			if sum(binary[:,yy]==0) > binary.shape[0]-10: 
+				maxy = yy
+				break
 
 		print(minx, maxx, miny, maxy)
-		ax3.set_title('Foreground')
-		ax3.imshow(img[minx:maxx+1, miny:maxy+1], cmap='Greys_r')
-		ax3.axis('off')
-
+		# ax3.set_title('Foreground')
+		# ax3.imshow(img[minx:maxx+1, miny:maxy+1], cmap='Greys_r')
+		# ax3.axis('off')
+		#.pickle
 		img = img.astype(np.float32)
 		img = scipy.misc.imresize(img[minx:maxx+1, miny:maxy+1], (224, 224), interp='cubic')
-		with open(join(preprocesspath, names[0])+'.pickle', 'wb') as outfile:
-		  cPickle.dump(img, outfile) 
-		ax4.set_title('Resize')
-		ax4.imshow(img, cmap='Greys_r')
-		ax4.axis('off')
 
+		fname = name.split('.')[0]
+		print fname
+
+		# if os.path.isdir(join(preprocesspath, fname)+'.pickle'):
+		# 	os.mknod(join(preprocesspath, fname)+'.pickle')
+
+		with open(join(preprocesspath, fname)+'.pickle', 'w') as pickle_file:
+			cPickle.dump(img, pickle_file)
+
+		# ax4.set_title('Resize')
+		# ax4.imshow(img, cmap='Greys_r')
+		# ax4.axis('off')
+
+		#.normpickle
 		img = img.astype(np.float32)
 		img -= np.mean(img)
 		img /= np.std(img)
-		ax5.set_title('Norm')
-		ax5.imshow(img, cmap='Greys_r')
-		ax5.axis('off')
-		with open(join(preprocesspath, names[0])+'norm.pickle', 'wb') as outfile:
-			cPickle.dump(img, outfile)
-			#imgshape = img.shape
+		# ax5.set_title('Norm')
+		# ax5.imshow(img, cmap='Greys_r')
+		# ax5.axis('off')
+		with open(join(preprocesspath, fname)+'norm.pickle', 'w') as pickle_file:
+			cPickle.dump(img, pickle_file)
+		# cPickle.dump(img, join(preprocesspath, name)+'norm.pickle')
+		#imgshape = img.shape
 
-		img = np.fliplr(img)
-		ax6.set_title('Flip')
-		ax6.imshow(img, cmap='Greys_r')
-		ax6.axis('off')
+		# img = np.fliplr(img)
+		# ax6.set_title('Flip')
+		# ax6.imshow(img, cmap='Greys_r')
+		# ax6.axis('off')
 
-		num_rot = np.random.choice(4)               #rotate 90 randomly
-		img = np.rot90(img, num_rot)
-		ax7.set_title('Rotation')
-		ax7.imshow(img, cmap='Greys_r')
-		ax7.axis('off')
-		fig.savefig(join(preprocesspath, names[0])+'.jpg')
-		plt.close(fig)
+		# num_rot = np.random.choice(4)               #rotate 90 randomly
+		# img = np.rot90(img, num_rot)
+		# ax7.set_title('Rotation')
+		# ax7.imshow(img, cmap='Greys_r')
+		# ax7.axis('off')
+		# fig.savefig(join(preprocesspath, name)+'.jpg')
+		# plt.close(fig)
 
 def cvsplit(fold, totalfold, mydict):
-  '''get the split of train and test
-  fold is the returned fold th data, from 0 to totalfold-1
-  total fold is for the cross validation
-  mydict is the return dict from readlabel'''
+	'''get the split of train and test
+	fold is the returned fold th data, from 0 to totalfold-1
+	total fold is for the cross validation
+	mydict is the return dict from readlabel'''
 	skf = StratifiedKFold(n_splits=totalfold)  # default shuffle is false, okay!
 	#readdicom(mydict)
 	y = mydict.values()
@@ -236,9 +247,9 @@ def loaddata(fold, totalfold, usedream=True, aug=True):
 	return traindata, trainlabel, testdata, testlabel
 
 def loaddataenhance(fold, totalfold, usedream=True, loadtrain=False, aug=True):
-  '''get the fold th train and  test data from inbreast
-  fold is the returned fold th data, from 0 to totalfold-1
-  total fold is for the cross validation'''
+	'''get the fold th train and  test data from inbreast
+	fold is the returned fold th data, from 0 to totalfold-1
+	total fold is for the cross validation'''
 	mydict = readlabel()
 	mydictkey = mydict.keys()
 	mydictvalue = mydict.values()
@@ -308,12 +319,13 @@ def loaddataenhance(fold, totalfold, usedream=True, loadtrain=False, aug=True):
 	return traindata, trainlabel, valdata, vallabel, testdata, testlabel
 
 ### Outside data source from dream challenge
-dreampreprocesspath = '../dockerdream/preprocessedData/'
-dreamcsv1 = '../dockerdream/metadata/images_crosswalk.tsv'
-dreamcsv2 = '../dockerdream/metadata/exams_metadata.tsv'
+dreamtrainpath = '../trainData/'
+dreamsavepth = '../Data/preprocessedData/'
+dreamcsv1 = '../metaData/images_crosswalk.tsv'
+dreamcsv2 = '../metaData/exams_metadata.tsv'
 
 def extractdreamdata(aug=True):
-  """
+	"""
 	Goes through data folder and divides train/val.
 	There should be two csv files.  The first will relate the filename
 	to the actual patient ID and L/R side, then the second csv file
@@ -325,17 +337,14 @@ def extractdreamdata(aug=True):
 	dict_img_to_patside, counter = {}, 0
 	#checklabel = {}
 	with open(path_csv_crosswalk, 'r') as file_crosswalk:
-		reader_crosswalk = csv.reader(file_crosswalk, delimiter='\t')
+		reader_crosswalk = csv.reader(file_crosswalk, delimiter='\t')[1:]
 		for row in reader_crosswalk:
-			if counter == 0:
-				counter += 1
-				continue
 			dict_img_to_patside[row[5].strip()] = (row[0].strip(), row[4].strip())
-	#    checklabel[row[5].strip()] = int(row[6])
-	# Now, let's map the tuple to cancer or non-cancer.
+			# checklabel[row[5].strip()] = int(row[6])
+			# Now, let's map the tuple to cancer or non-cancer.
 	dict_tuple_to_cancer, counter = {}, 0
 	with open(path_csv_metadata, 'r') as file_metadata:
-		reader_metadata = csv.reader(file_metadata, delimiter='\t')
+		reader_metadata = csv.reader(file_metadata, delimiter='\t')[1:]
 		for row in reader_metadata:
 			if counter == 0:
 				counter += 1
@@ -352,9 +361,9 @@ def extractdreamdata(aug=True):
 		assert(dict_tuple_to_cancer[dict_img_to_patside[img_name]] ==0 or 
 		dict_tuple_to_cancer[dict_img_to_patside[img_name]] == 1)
 		Y_tot.append(dict_tuple_to_cancer[dict_img_to_patside[img_name]])
-		#if checklabel[img_name] != dict_tuple_to_cancer[dict_img_to_patside[img_name]]:
-		#  print img_name, checklabel[img_name], dict_tuple_to_cancer[dict_img_to_patside[img_name]]
-	# Making train/val split and returning.
+		# if checklabel[img_name] != dict_tuple_to_cancer[dict_img_to_patside[img_name]]:
+		# 	print img_name, checklabel[img_name], dict_tuple_to_cancer[dict_img_to_patside[img_name]]
+	#Making train/val split and returning.
 	#X_tr, X_te, Y_tr, Y_te = train_test_split(X_tot, Y_tot, test_size=0.2, random_state=1) # 0.2
 	if aug == True:
 		trainx, trainy = np.zeros((24*len(X_tot), 224, 224)), np.zeros((24*len(Y_tot),))
@@ -376,8 +385,9 @@ def extractdreamdata(aug=True):
 	return trainx, trainy #dict_img_to_patside
 
 if __name__ == '__main__':
-	traindata, trainlabel, testdata, testlabel = loaddata(0, 5)
-	print(sum(trainlabel), sum(testlabel))
+	readdicom("../trainingData/")
+	# traindata, trainlabel, testdata, testlabel = loaddata(0, 5)
+	# print(sum(trainlabel), sum(testlabel))
 
-	traindata, trainlabel, valdata, vallabel, testdata, testlabel = loaddataenhance(0, 5)
-	print(sum(trainlabel), sum(vallabel), sum(testlabel))
+	# traindata, trainlabel, valdata, vallabel, testdata, testlabel = loaddataenhance(0, 5)
+	# print(sum(trainlabel), sum(vallabel), sum(testlabel))
